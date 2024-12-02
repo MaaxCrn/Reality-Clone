dd3c70c80bc9624195f3f7ed2e98ce247b06fd99
Brounix <brounix23@gmail.com>
2024-12-02 16:40:20 +0100

    save pos rot...

--- App/reality_clone/lib/ui/picturepage.dart
+++ App/reality_clone/lib/ui/picturepage.dart
@@ -5,11 +5,14 @@ import 'package:ar_flutter_plugin_flutterflow/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin_flutterflow/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin_flutterflow/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin_flutterflow/managers/ar_session_manager.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin_flutterflow/ar_flutter_plugin.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:vector_math/vector_math_64.dart';
+
 
 import '../domain/capturedphoto.dart';
 import 'imagelistepage.dart';
@@ -25,6 +28,10 @@ class _ARPageState extends State<ARPage> {
   late ARSessionManager arSessionManager;
   late ARObjectManager arObjectManager;
 
+  late CameraController _cameraController;
+  List<CameraDescription> _cameras = [];
+  CameraDescription? _camera;
+
   List<CapturedPhoto> capturedPhotos = [];
   int _photoCount = 0;
   GlobalKey _repaintKey = GlobalKey();
@@ -32,11 +39,48 @@ class _ARPageState extends State<ARPage> {
   @override
   void dispose() {
     arSessionManager.dispose();
+    _initializeCamera();
     super.dispose();
   }
 
+  Future<void> _initializeCamera() async {
+    _cameras = await availableCameras();
+
+    _camera = _cameras.firstWhere(
+          (camera) => camera.lensDirection == CameraLensDirection.back,
+      orElse: () => _cameras.first,
+    );
+
+    _cameraController = CameraController(
+      _camera!,
+      ResolutionPreset.high,
+    );
+
+    await _cameraController.initialize();
+
+    _getCameraInfo();
+  }
+
+  Future<void> _getCameraInfo() async {
+    try {
+      final cameraId = 1;
+      final model = _camera?.lensDirection.toString() ?? 'Unknown';
+      final width = _cameraController.value.previewSize?.width ?? 0.0;
+      final height = _cameraController.value.previewSize?.height ?? 0.0;
+
+      debugPrint('Camera ID: $cameraId');
+      debugPrint('Camera Model: $model');
+      debugPrint('Camera Width: $width');
+      debugPrint('Camera Height: $height');
+
+    } catch (e) {
+      debugPrint('Error retrieving camera info: $e');
+    }
+  }
+
   Future<void> _capturePhotosWithPositions() async {
-    int photosToCapture = 50 + (100 - 50) * (DateTime.now().millisecondsSinceEpoch % 50) ~/ 50;
+    int photosToCapture =
+        50 + (100 - 50) * (DateTime.now().millisecondsSinceEpoch % 50) ~/ 50;
 
     for (int i = 0; i < 1; i++) {
       final position = await _getCameraPosition();
@@ -60,15 +104,29 @@ class _ARPageState extends State<ARPage> {
 
   Future<void> _takeScreenshot(Map<String, double> position) async {
     try {
-      RenderRepaintBoundary boundary = _repaintKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      RenderRepaintBoundary boundary = _repaintKey.currentContext!
+          .findRenderObject() as RenderRepaintBoundary;
       var image = await boundary.toImage(pixelRatio: 3.0);
+
+      final cameraPose = await arSessionManager.getCameraPose();
+      final rotationMatrix = cameraPose!.getRotation();
+      final quaternion = Quaternion.fromRotation(rotationMatrix);
+
+      Map<String, double> rotation = {
+        'qx': quaternion.x,
+        'qy': quaternion.y,
+        'qz': quaternion.z,
+        'qw': quaternion.w,
+      };
 
       image.toByteData(format: ImageByteFormat.png).then((byteData) async {
         if (byteData != null) {
           final directory = await getTemporaryDirectory();
-          final filePath = '${directory.path}/screenshot_${_photoCount + 1}.png';
+          final filePath = '${directory.path}/image_${_photoCount + 1}.jpg';
+          final imageName = 'image_${_photoCount + 1}.jpg';
 
-          final List<int> compressedBytes = await FlutterImageCompress.compressWithList(
+          final List<int> compressedBytes =
+          await FlutterImageCompress.compressWithList(
             byteData.buffer.asUint8List(),
             minWidth: 800,
             minHeight: 600,
@@ -78,7 +136,15 @@ class _ARPageState extends State<ARPage> {
           final file = File(filePath);
           await file.writeAsBytes(compressedBytes);
 
-          capturedPhotos.add(CapturedPhoto(path: filePath, position: position));
+          capturedPhotos.add(
+            CapturedPhoto(
+              id: _photoCount + 1,
+              path: filePath,
+              name: imageName,
+              position: position,
+              rotation: rotation,
+            ),
+          );
           _photoCount++;
           debugPrint('Screenshot saved to $filePath');
         }
@@ -88,6 +154,7 @@ class _ARPageState extends State<ARPage> {
     }
   }
 
+
   void _showCapturedPhotos() {
     Navigator.push(
       context,
@@ -96,6 +163,59 @@ class _ARPageState extends State<ARPage> {
       ),
     );
   }
+
+  Future<void> _saveAllCapturedPhotosDataToFile() async {
+    try {
+      StringBuffer dataBuffer = StringBuffer();
+
+      for (var photo in capturedPhotos) {
+        final cameraPose = await arSessionManager.getCameraPose();
+
+        final cameraPosition = Vector3(photo.position['x']!, photo.position['y']!, photo.position['z']!);
+        final rotationMatrix = cameraPose!.getRotation();
+        final quaternion = Quaternion.fromRotation(rotationMatrix);
+
+        int imageId = photo.id;
+        String cameraId = '1';
+        String imageName = photo.name;
+
+        dataBuffer.write('$imageId,'
+            '${quaternion.w},'
+            '${quaternion.x},'
+            '${quaternion.y},'
+            '${quaternion.z},'
+            '${cameraPosition.x},'
+            '${cameraPosition.y},'
+            '${cameraPosition.z},'
+            '$cameraId,'
+            '$imageName\n');
+      }
+
+      final directory = await getExternalStorageDirectory();
+      if (directory == null) {
+        debugPrint("Error: Unable to access external storage");
+        return;
+      }
+
+      final downloadDirectory = Directory('${directory.path}/Download');
+      if (!await downloadDirectory.exists()) {
+        await downloadDirectory.create(recursive: true);
+      }
+
+      final filePath = '${downloadDirectory.path}/all_captured_photos_data_${DateTime.now().millisecondsSinceEpoch}.txt';
+      final file = File(filePath);
+
+      await file.writeAsString(dataBuffer.toString());
+
+      await file.writeAsString(dataBuffer.toString());
+
+      debugPrint('All camera data saved to ${file.path}');
+    } catch (e) {
+      debugPrint("Error saving camera data for all photos: $e");
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -139,25 +259,40 @@ class _ARPageState extends State<ARPage> {
               ),
             ),
           ),
+          Positioned(
+            bottom: 180,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: _saveAllCapturedPhotosDataToFile,
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(20),
+                ),
+                child: const Icon(Icons.save, size: 30),
+              ),
+            ),
+          )
+
         ],
       ),
     );
   }
 
   void onARViewCreated(
-      ARSessionManager arSessionManager,
-      ARObjectManager arObjectManager,
-      ARAnchorManager arAnchorManager,
-      ARLocationManager arLocationManager,
-      ) {
+    ARSessionManager arSessionManager,
+    ARObjectManager arObjectManager,
+    ARAnchorManager arAnchorManager,
+    ARLocationManager arLocationManager,
+  ) {
     this.arSessionManager = arSessionManager;
 
     this.arSessionManager.onInitialize(
-      showFeaturePoints: false,
-      showPlanes: false,
-      showWorldOrigin: false,
-      handleTaps: false,
-    );
-
+          showFeaturePoints: false,
+          showPlanes: false,
+          showWorldOrigin: false,
+          handleTaps: false,
+        );
   }
 }
